f4498f3205e5ebad250fd27ebbdab3645110a4f7
Somnath Kundu <kundusomnath610@gmail.com>
2025-04-01 12:07:07 +0530

    commit

--- src/main/java/org/example/copyConstructor.java
+++ src/main/java/org/example/copyConstructor.java
@@ -5,10 +5,12 @@ public class copyConstructor {
         Candidate c1 = new Candidate("Somnath", "cse", 2022, 8.0, "jis");
         Candidate c2 = new Candidate("Somnath kundu", "cse", 2022, 8.6, "jis");
         Candidate c3 = new Candidate("Rajesh", "cse", 2022, 8.9, "IEM");
+        Candidate c4 = new Candidate("Rana", "cse", 2024, 8.65, "Techno India");
 
         c1.Display();
-        c2.Display();
+        c2.Display(); 
         c3.Display();
+        c4.Display();
     }
 
 }
@@ -32,5 +34,6 @@ class Candidate {
         System.out.println("The branch is: " + branch);
         System.out.println("The cgpa is: " + cgpa);
         System.out.println("The institute is: " + insitute);
+        System.out.println();
     }
 }
