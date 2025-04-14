fcd7c226db43c1abce840202b0629300ab84078b
Somnath Kundu <kundusomnath610@gmail.com>
2025-04-14 23:31:13 +0530

    commit

--- src/main/java/org/example/Mobile.java
+++ src/main/java/org/example/Mobile.java
@@ -1,2 +1,15 @@
-package org.example;public class Mobile {
+package org.example;
+
+public class Mobile {
+    public static void main(String[] kod) {
+        String s1 = "Somnath";
+        System.out.println(s1);
+
+        String s2 = new String("Kundu");
+        System.out.println(s2);
+
+        char[] ch = {'s', 'o', 'm', 'n', 'a', 't', 'h', ' ', 'K', 'u', 'n', 'd', 'u'};
+        String s3 = new String(ch);
+        System.out.println(ch);
+    }
 }
--- src/main/java/org/example/ThreeDArray.java
+++ src/main/java/org/example/ThreeDArray.java
@@ -1,8 +1,52 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ThreeDArray {
     public static void main(String[] args) {
-        int[][][] array = new int[2][4][5];
+        Scanner scanner = new Scanner(System.in);
+
+        // Step 1: Ask for array size
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+
+        // Step 2 & 3: Create and populate the array
+        int[] original = new int[size];
+        System.out.print("Enter " + size + " digits (no spaces): ");
+        String input = scanner.next();
+
+        for (int i = 0; i < size; i++) {
+            original[i] = Character.getNumericValue(input.charAt(i));
+        }
+
+        // Copy for reversal
+        int[] reversed = new int[size];
+        for (int i = 0; i < size; i++) {
+            reversed[i] = original[size - 1 - i];
+        }
+
+        // Step 5: Print arrays
+        System.out.println("Original array: " + Arrays.toString(original));
+        System.out.print("Reversed array: [");
+        for (int i = 0; i < reversed.length; i++) {
+            System.out.print(reversed[i]);
+            if (i < reversed.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+
+        scanner.close();
+    }    
+}
+
+
+
+
+/*
+ * 
+ * int[][][] array = new int[2][4][5];
 
         array[0][0][0] = 21;
         array[0][0][1] = 27;
@@ -29,5 +73,5 @@ public class ThreeDArray {
             System.out.println();
         }
         System.out.println();
-    }    
-}
+ * 
+ */
