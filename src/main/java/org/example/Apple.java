68b1e5bd5a2c68519638dda867ac1339a436d8a6
Somnath Kundu <kundusomnath610@gmail.com>
2025-04-15 13:00:44 +0530

    commit

--- src/main/java/org/example/Apple.java
+++ src/main/java/org/example/Apple.java
@@ -1,2 +1,15 @@
-package org.example;public class Apple {
+package org.example;
+
+import java.util.Scanner;
+
+public class Apple {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String og = scanner.nextLine();
+        String suf = scanner.nextLine();
+
+        String userName = og.concat(suf);
+        System.out.println("Original Username: " + og);
+        System.out.println("New User: " + " " +userName);
+    }
 }
