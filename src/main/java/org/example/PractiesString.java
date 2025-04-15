07b014ce6bf87f78b2faf75e28d0225f61ba6744
Somnath Kundu <kundusomnath610@gmail.com>
2025-04-15 12:21:25 +0530

    commit

--- src/main/java/org/example/PractiesString.java
+++ src/main/java/org/example/PractiesString.java
@@ -1,2 +1,17 @@
-package org.example;public class PractiesString {
+package org.example;
+
+public class PractiesString {
+    public static void main(String[] args) {
+        String str1 = "Kod";
+        String str2 = new String("Apple");
+
+        String str3 = "Kod";
+        String str4 = new String("Apple");
+
+        System.out.println(str1 == str3);
+        System.out.println(str1.equals(str3));
+
+        System.out.println(str2 == str4);
+        System.out.println(str2.equals(str2));
+    }
 }
