626f848099f749ee1adb8d10002b6147848434b2
Somnath Kundu <kundusomnath610@gmail.com>
2025-04-16 12:35:18 +0530

    commit

--- src/main/java/StringIndexBound.java
+++ src/main/java/StringIndexBound.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class StringIndexBound {
+import java.util.Scanner;
+
+public class StringIndexBound {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        int idx = scanner.nextInt();
+
+        if (idx < str.length()) {
+            char ch = str.charAt(idx);
+            System.out.println("The index of the first occurrence is " + ch);
+        } else {
+            System.out.println("Invalid index.");
+        }
+    }
 }
