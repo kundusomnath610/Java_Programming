918c9079e55f22dfb8cb9c2d3e25302bf697f182
Somnath Kundu <kundusomnath610@gmail.com>
2025-04-21 15:30:14 +0530

    commit

--- src/main/java/org/example/CheckDuplicate.java
+++ src/main/java/org/example/CheckDuplicate.java
@@ -1,21 +1,14 @@
 package org.example;
 
-import java.util.HashSet;
 
+/*
+ * if a char is seen then it return true.
+ * if a char is not seen then return false.
+ * input = "banana" // return true
+ * input = "Somnath" // return false.
+ */
 public class CheckDuplicate {
     public static void main(String[] args) {
-        String str = "mac"; // should return false
-        System.out.println(duplicateCharacter(str));
-    }
-
-    public static boolean duplicateCharacter(String str) {
-        HashSet<Character> seen = new HashSet<>();
-        for (char ch : str.toCharArray()) {
-            if (seen.contains(ch)) {
-                return true; // duplicate found
-            }
-            seen.add(ch);
-        }
-        return false; // no duplicates
+        
     }
 }
