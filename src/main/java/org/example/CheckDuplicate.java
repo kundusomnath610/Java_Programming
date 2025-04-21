5aed03a518730aef4b01e3a2e9b54a24f6e51a64
Somnath Kundu <kundusomnath610@gmail.com>
2025-04-21 15:55:11 +0530

    commit

--- src/main/java/org/example/CheckDuplicate.java
+++ src/main/java/org/example/CheckDuplicate.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.HashSet;
 
 /*
  * if a char is seen then it return true.
@@ -9,6 +10,18 @@ package org.example;
  */
 public class CheckDuplicate {
     public static void main(String[] args) {
-        
+        String str = "somnath";
+        System.out.println(duplicte(str));
+    }
+
+    public static boolean duplicte(String str) {
+        HashSet<Character> seen = new HashSet<>();
+        for (char ch : str.toCharArray()) {
+            if (seen.contains(ch)) {
+                return true;
+            }
+            seen.add(ch);
+        }
+        return false;
     }
 }
