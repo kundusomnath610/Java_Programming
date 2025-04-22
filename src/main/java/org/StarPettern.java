7e7bf22f641ee9522fb3582138bb5f87bdf43858
Somnath Kundu <kundusomnath610@gmail.com>
2025-04-22 13:22:01 +0530

    commit

---
+++ src/main/java/org/StarPettern.java
@@ -0,0 +1,17 @@
+package org;
+
+import java.util.Scanner;
+
+public class StarPettern {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        scn.close();
+    }
+}
