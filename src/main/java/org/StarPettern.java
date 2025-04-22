14b49555f2e2596e47ac08b06047e8f3f7bc4d43
Somnath Kundu <kundusomnath610@gmail.com>
2025-04-22 13:38:31 +0530

    commit

--- src/main/java/org/StarPettern.java
+++ src/main/java/org/StarPettern.java
@@ -6,12 +6,27 @@ public class StarPettern {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print("*");
+        System.out.println();
+        
+        for(int i = 0; i < n; i++) {
+            for (int j = i; j >= 0; j--) {
+                System.out.print("*" + " ");
             }
             System.out.println();
         }
         scn.close();
     }
 }
+
+
+/*
+ * 
+ * for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        scn.close();
+ * 
+ */
