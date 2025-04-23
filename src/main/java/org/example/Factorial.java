a6022dc56733f4665ca90d30b5f9226f139489c7
Somnath Kundu <kundusomnath610@gmail.com>
2025-04-23 12:18:33 +0530

    Initial commit

--- src/main/java/org/example/Factorial.java
+++ src/main/java/org/example/Factorial.java
@@ -2,17 +2,21 @@ package org.example;
 
 import java.util.Scanner;
 
-public class Factorial {
+public class Factorial { // through while loop
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a number:- ");
         int number = scanner.nextInt();
         int fact = 1;
 
-        while (number > 0) {
-            fact = number * fact;
+        // while (number > 0) {
+        //     fact = number * fact;
+        // }
+
+        for (int i = number; i >= 1; i--) {
+            fact = fact * i;
         }
-        System.out.println("The factorial is number is :" + fact);
-        scanner.close();
+         System.out.println("The factorial is number is :" + fact);
+         scanner.close();
     }
 }
