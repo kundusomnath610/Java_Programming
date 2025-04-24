c6a58003ffc19d675a59a5fa25612eb76333b771
Somnath Kundu <kundusomnath610@gmail.com>
2025-04-24 12:15:41 +0530

    commit

--- src/main/java/org/example/Factorial.java
+++ src/main/java/org/example/Factorial.java
@@ -12,10 +12,10 @@ public class Factorial { // through while loop
         // while (number > 0) {
         //     fact = number * fact;
         // }
-
-        for (int i = number; i >= 1; i--) {
-            fact = fact * i;
-        }
+                for (int i = number; i >= 1; i--) {
+                    fact = fact * i;
+                }
+        
          System.out.println("The factorial is number is :" + fact);
          scanner.close();
     }
