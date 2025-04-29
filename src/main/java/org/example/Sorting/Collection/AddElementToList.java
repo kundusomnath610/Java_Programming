1bd7b8cf6c41fc854b9d4f491ef163b6177316f3
Somnath Kundu <kundusomnath610@gmail.com>
2025-04-29 13:10:37 +0530

    commit

---
+++ src/main/java/org/example/Sorting/Collection/AddElementToList.java
@@ -0,0 +1,28 @@
+package org.example.Sorting.Collection;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AddElementToList {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter a list Size:- ");
+        int n = scn.nextInt();
+        ArrayList list = new ArrayList();
+
+
+        // Add element to the list
+        System.out.println("Enter a Number:- ");
+        for (int i = 1; i <= n; i++) {
+            int num = scn.nextInt();
+            list.add(num);
+        }
+
+        System.out.println("Size is: " + list.size());
+        System.out.print("Element is: ");
+        for (Object o : list) {
+            System.out.print(o + " ");
+        }
+        scn.close();
+    }
+}
