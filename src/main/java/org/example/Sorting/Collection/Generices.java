c033be2df72e45dbc897bcfbfde2a1cf70a2e582
Somnath Kundu <kundusomnath610@gmail.com>
2025-05-02 12:32:02 +0530

    commit

---
+++ src/main/java/org/example/Sorting/Collection/Generices.java
@@ -0,0 +1,17 @@
+package org.example.Sorting.Collection;
+
+import java.util.ArrayList;
+
+public class Generices {
+    public static void main(String[] args) {
+        ArrayList <String> list = new ArrayList<String>();
+
+        list.add("Somnath");
+        list.add("12344");
+        list.add("Code");
+        list.add("Laptop");
+
+        System.out.println("The list is: " + list);
+
+    }
+}
