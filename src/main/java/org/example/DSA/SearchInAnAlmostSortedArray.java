dcbaa2688e8b6d0a316cd31a79dcca3548ee0584
Somnath Kundu <kundusomnath610@gmail.com>
2025-05-05 22:40:03 +0530

    commit

---
+++ src/main/java/org/example/DSA/SearchInAnAlmostSortedArray.java
@@ -0,0 +1,11 @@
+package org.example.DSA;
+
+public class SearchInAnAlmostSortedArray {
+    public static void main(String[] args) {
+        
+    }
+
+    public static int almostSortedArray(int[] arr, int target) {
+        
+    }
+}
