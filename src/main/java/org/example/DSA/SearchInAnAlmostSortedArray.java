84e9c9740022a1a0b962c74b2fe2bbb34fc6db21
Somnath Kundu <kundusomnath610@gmail.com>
2025-05-05 22:51:03 +0530

    commit

--- src/main/java/org/example/DSA/SearchInAnAlmostSortedArray.java
+++ src/main/java/org/example/DSA/SearchInAnAlmostSortedArray.java
@@ -2,31 +2,28 @@ package org.example.DSA;
 
 public class SearchInAnAlmostSortedArray {
     public static void main(String[] args) {
-        
+        int[] arr = {10, 20, 3, 40, 50, 80, 70};
+        System.out.println("The Array Index present in: " + almostSortedArray(arr, 3));
     }
 
     public static int almostSortedArray(int[] arr, int target) {
         // Binary search algorithm..
 
         int n = arr.length;
-
-        int low = 0;
-        int high = n - 1;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (arr[mid] == target) return mid;
-
-            if (arr[mid - 1] == target) return mid - 1;
-            if (arr[mid + 1] == target) return mid + 1;
-
-            if (arr[mid] < target) {
-                low = mid + 1;
-                return mid + 2;
-            } else {
-                high = mid - 1;
-                return mid - 2;
+        
+        int low = 0, high = n-1;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            
+            if(arr[mid] == target){
+                return mid;
+            }else if(arr[mid] < target){
+                //just add this condition for just sorted
+                if(mid - 1 >= 0 && arr[mid-1] == target) return mid - 1;
+                low = mid+1;
+            }else{
+                if(mid + 1 < n && arr[mid+1] == target) return mid + 1;
+                high = mid -1;
             }
         }
         return -1;
