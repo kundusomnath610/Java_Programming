df2f241d05592df50104a4bff9906c8666aa1751
Somnath Kundu <kundusomnath610@gmail.com>
2025-05-09 23:29:47 +0530

    commit

--- src/main/java/org/example/DSA/DuplicateElement.java
+++ src/main/java/org/example/DSA/DuplicateElement.java
@@ -5,22 +5,32 @@ import java.util.*;
 public class DuplicateElement {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        System.out.println("enter a array:- ");
+        System.out.print("Enter the size of the array: ");
         int n = scn.nextInt();
         int[] array = new int[n];
+
+        System.out.println("Enter array elements:");
         for (int i = 0; i < n; i++) {
             array[i] = scn.nextInt();
         }
+
         Arrays.sort(array);
 
-        for (int i = 0; i < array.length; i++) {
+        boolean hasDuplicates = false;
+        for (int i = 0; i < array.length - 1; i++) {
             if (array[i] == array[i + 1]) {
-                System.out.println("The array is duplicate is " + array[i] + ".");
-            } else {
-                System.out.println("this is not match.");
+                // Print only the first occurrence of each duplicate
+                if (i == 0 || array[i] != array[i - 1]) {
+                    System.out.println("Duplicate element found: " + array[i]);
+                    hasDuplicates = true;
+                }
             }
         }
+
+        if (!hasDuplicates) {
+            System.out.println("No duplicate elements found.");
+        }
+
         scn.close();
     }
 }
-
