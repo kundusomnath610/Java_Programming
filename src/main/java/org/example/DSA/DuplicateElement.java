1ad6f7777b2b05aae9d2ea1a678639ae689212d7
Somnath Kundu <kundusomnath610@gmail.com>
2025-05-09 23:23:34 +0530

    commit

--- src/main/java/org/example/DSA/DuplicateElement.java
+++ src/main/java/org/example/DSA/DuplicateElement.java
@@ -1,10 +1,26 @@
 package org.example.DSA;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class DuplicateElement {
     public static void main(String[] args) {
-        
+        Scanner scn = new Scanner(System.in);
+        System.out.println("enter a array:- ");
+        int n = scn.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = scn.nextInt();
+        }
+        Arrays.sort(array);
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == array[i + 1]) {
+                System.out.println("The array is duplicate is " + array[i] + ".");
+            } else {
+                System.out.println("this is not match.");
+            }
+        }
+        scn.close();
     }
 }
 
