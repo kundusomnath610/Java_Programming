9b4651a45bd5868783e5a905fe3522b3dee8031a
Somnath Kundu <kundusomnath610@gmail.com>
2025-05-14 14:59:32 +0530

    commit

--- src/main/java/org/example/DSA/compearableInterface.java
+++ src/main/java/org/example/DSA/compearableInterface.java
@@ -1,32 +1,55 @@
 package org.example.DSA;
 
+import java.time.Period;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
 
 public class compearableInterface {
     public static void main(String[] args) {
-        ArrayList<Object> list  = new ArrayList<>();
+        ArrayList<Person> list  = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            String name = sc.next();
+            int age = sc.nextInt();
+
+            Person p = new Person(name, age);
+            list.add(p);
+        }
+
+        Arrange arrange = new Arrange();
+        Collections.sort(list, arrange);
+
+        System.out.println("People sorted by age: ");
+        for(Object o : list) {
+            System.out.println(o);
+        }
     }
 
-    class Student implements Comparable {
-        int roll;
-        String name;
-        float cgpa;
+    class Arrange implements Comparator<P> {
+        public int compear(Person o1, Person o2) {
+            if (o1.age < o2.age)
+                return 1;
+            else 
+                return 0;
+        }
+    } 
 
-        public Student (int roll, String name, float cgpa) {
-            this.roll = roll;
+    class Person {
+        String name;
+        int age;
+
+        public Person (String name, int age) {
             this.name = name;
-            this.cgpa = cgpa;
+            this.age = age;
         }
 
         @Override
         public String toString() {
-            return "Student [roll=" + roll + ", name=" + name + ", cgpa=" + cgpa + "]";
-        }
-
-        @Override
-        public int compareTo(Object o) {
-            // TODO Auto-generated method stub
-            throw new UnsupportedOperationException("Unimplemented method 'compareTo'");
+            return "Person {name=" + name + ", age=" + age + "}";
         }
     }
 }
