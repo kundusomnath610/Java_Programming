70f5dc85de7c5b133ff90bf5a5128113540ccb50
Somnath Kundu <kundusomnath610@gmail.com>
2025-05-16 14:55:08 +0530

    commit

--- src/main/java/org/example/DSA/WeakMap.java
+++ src/main/java/org/example/DSA/WeakMap.java
@@ -8,11 +8,13 @@ public class WeakMap {
         String obj1 = "Java";
         String obj2 = "Python";
         String obj3 = "C++";
+        String obj4 = "Go Lang";
 
         HashMap map = new HashMap();
         map.put(1, obj1);
         map.put(2, obj2);
         map.put(3, obj3);
+        map.put(4, obj4);
 
         System.out.println(map);
 
