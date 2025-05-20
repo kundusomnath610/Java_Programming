d822cb3811c0cb72b5564d6f9f85379b19524c4f
Somnath Kundu <kundusomnath610@gmail.com>
2025-05-20 14:59:26 +0530

    commit

--- src/main/java/org/example/DSA/StackImpl.java
+++ src/main/java/org/example/DSA/StackImpl.java
@@ -4,23 +4,31 @@ import java.util.ArrayList;
 
 public class StackImpl {
     public static void main(String[] args) {
-        
-        ArrayList<Integer> al = new ArrayList<>();
-        al.add(10); // 1
-        al.add(20); // 2
-        al.add(30); // 3
-        al.add(40); // 4
-        al.add(50); // 5
-        al.add(60); // 6
-
-        System.out.println(al);
 
        
-        al.remove(5);
-        al.remove(2);
+    }
 
+    int top = -1;
+    int[] stack;
+    int capacity = 5;
 
-        System.out.println(al);
-        System.out.println(al.size());
+    public void push(int num) {
+        if (top < (capacity - 1)) {
+            top++;
+            stack[top] = num;
+        } else {
+            System.out.println("Cannot push, Max capacity reached");
+        }
+    }
+
+    public int pop() {
+        if (top >= 0) {
+            int ele = stack[top];
+            top--;
+            return ele;
+        } else {
+            System.out.println("Stack is empty..");
+            return -1;
+        }
     }
 }
