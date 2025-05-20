13b6b7e932c0f2d7874313746c342a27362c6a3f
Somnath Kundu <kundusomnath610@gmail.com>
2025-05-20 14:21:12 +0530

    commit

--- src/main/java/org/example/DSA/StackImpl.java
+++ src/main/java/org/example/DSA/StackImpl.java
@@ -8,5 +8,9 @@ public class StackImpl {
         ArrayList<Integer> al = new ArrayList<>();
         al.add(10);
         al.add(20);
+        al.add(30);
+        al.add(40);
+        al.add(50);
+        al.add(60);
     }
 }
