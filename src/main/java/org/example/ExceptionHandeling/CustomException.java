921893209f9545bc8e4eaf1b86dbe3a0776ceaf9
Somnath Kundu <kundusomnath610@gmail.com>
2025-05-21 15:57:57 +0530

    commit

--- src/main/java/org/example/ExceptionHandeling/CustomException.java
+++ src/main/java/org/example/ExceptionHandeling/CustomException.java
@@ -3,18 +3,28 @@ package org.example.ExceptionHandeling;
 import java.util.Scanner;
 
 public class CustomException {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws EligibilityException {
         Scanner scn = new Scanner(System.in);
         System.out.println("Enter a Branch");
         String branch = scn.next();
         System.out.println("Please enter a marks");
         int mark = scn.nextInt();
-        
-        if (branch.equals("cse") && mark >= 80) 
-            System.out.println("Congrats you are eligible");
-        else 
-            if (branch.equals("cse") == false)
-            throw new EligibilityException("Only CSE is allowed..");
-    }
 
+        if (branch.equals("cse") && mark >= 80) {
+            System.out.println("Congrats you are eligible!!!");
+        } else {
+            if (branch.equals("cse") == false) 
+                throw new EligibilityException("Only CSE allowed!!!");
+            if (mark < 80) 
+                throw new EligibilityException("Your marks lower then 80 percent"); 
+
+        }
+
+    }
 }
+
+    class EligibilityException extends Exception {
+        public EligibilityException(String msg) {
+            super(msg);
+        }
+    }
