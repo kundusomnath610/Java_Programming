6507b71ef67937a54fb989b6838bf6df530969b0
Somnath Kundu <kundusomnath610@gmail.com>
2025-06-03 14:48:14 +0530

    commit

--- src/main/java/org/example/SeriallizeDeserialize/FileHandling.java
+++ src/main/java/org/example/SeriallizeDeserialize/FileHandling.java
@@ -1,16 +1,29 @@
+/*
+ *  Creating the file in java..
+ * 
+ */
+
 package org.example.SeriallizeDeserialize;
 
 import java.io.File;
-import java.io.IOException;
+import java.io.FileWriter;
+
 
 public class FileHandling {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         File f = new File("Sample.txt");
-        if (f.exists() == true)
-            System.out.println("File is present");
-        else 
-            System.out.println("File not found");
-            f.createNewFile();
+
+
+        FileWriter writer = new FileWriter(f);
+        writer.write("Hello from javaScript");
+
+        writer.close();
+
+        // if (f.exists() == true)
+        //     System.out.println("File is present");
+        // else 
+        //     System.out.println("File not found");
+        //     f.createNewFile();
     }
 
 }
---
+++ src/main/java/org/example/SeriallizeDeserialize/Writethefile.java
@@ -0,0 +1,12 @@
+package org.example.SeriallizeDeserialize;
+
+import java.io.File;
+import java.io.FileWriter;
+
+public class Writethefile {
+    public static void main(String[] args) throws Exception{
+        File f1 = new File("Sample.txt");
+
+        
+    }
+}
