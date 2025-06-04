e40c672038767350f47ebfd8b54329da1cb538c4
Somnath Kundu <kundusomnath610@gmail.com>
2025-06-04 16:21:13 +0530

    commit

--- src/main/java/org/example/SingletonPattern/Calculator.java
+++ src/main/java/org/example/SingletonPattern/Calculator.java
@@ -3,19 +3,24 @@ package org.example.SingletonPattern;
 
 public class Calculator {
     public static void main(String[] args) {
-        Student s1 = new Student();
-        Student s2 = new Student();
-        Student s3 = new Student();
+        Student s1 = Student.getStudent();
+        Student s2 = Student.getStudent();
+        Student s3 = Student.getStudent();
 
-        System.out.println(Student.count);
+        System.out.println(s1.hashCode());
+        System.out.println(s2.hashCode());
+        System.out.println(s3.hashCode());
     }
 }
 
 class Student {
+    static Student st;
+    private Student() {
 
-    static int count = 0;
-    public Student() {
-        count++;
     }
-
+    public static Student getStudent() {
+        if (st == null) 
+            st = new Student();
+        return st;
+    }
 }
