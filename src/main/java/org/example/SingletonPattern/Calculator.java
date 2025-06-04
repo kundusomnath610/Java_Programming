77bf2099f3ab06c3413020670871ad0f073c03ee
Somnath Kundu <kundusomnath610@gmail.com>
2025-06-04 15:20:56 +0530

    commit

--- src/main/java/org/example/SingletonPattern/Calculator.java
+++ src/main/java/org/example/SingletonPattern/Calculator.java
@@ -6,7 +6,6 @@ public class Calculator {
         Student s1 = new Student();
         Student s2 = new Student();
         Student s3 = new Student();
-        Student s4 = new Student();
 
         System.out.println(Student.count);
     }
