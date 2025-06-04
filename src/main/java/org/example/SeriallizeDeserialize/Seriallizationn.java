1a5d4ee9676a73d2cbd20727ad68511ac11f0fae
Somnath Kundu <kundusomnath610@gmail.com>
2025-06-04 14:55:04 +0530

    commit

---
+++ src/main/java/org/example/SeriallizeDeserialize/Seriallizationn.java
@@ -0,0 +1,33 @@
+package org.example.SeriallizeDeserialize;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+
+public class Seriallizationn {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        FileInputStream fs = new FileInputStream("Output.txt");
+        ObjectInputStream ois = new ObjectInputStream(fs);
+
+        Student st = (Student) ois.readObject();
+        System.out.println(st);
+    }
+}
+
+class Student implements Serializable {
+    int roll;
+    String name;
+    float cgpa;
+    transient int otp;
+    public Student(int roll, String name, float cgpa, int otp) {
+            this.roll = roll;
+            this.name = name;
+            this.cgpa = cgpa;
+            this.otp = otp;
+    }
+    @Override
+    public String toString() {
+        return "Student(roll = " + roll + " Name = " + name + "cgpa " + cgpa + "OTP is: " + otp + ")";
+    }
+}
